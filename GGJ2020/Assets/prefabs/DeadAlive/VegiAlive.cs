5bf29c22ae1a91b41445b7a7a56b33dd8b588866
Gregory Fournier <gregory.fournier@logient.com>
2020-01-31 23:37:40 -0500

    A whole bunch of stuff

---
+++ GGJ2020/Assets/prefabs/DeadAlive/VegiAlive.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegiAlive : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _deadObject;
+
+    [SerializeField]
+    private GameObject _aliveObject;
+
+    [SerializeField]
+    private bool _isAlive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToggleObjects();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<DeadAliveCollider>() != null)
+        {
+            _isAlive = true;
+            ToggleObjects();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<DeadAliveCollider>() != null)
+        {
+            _isAlive = false;
+            ToggleObjects();
+        }
+    }
+
+    private void ToggleObjects()
+    {
+        _deadObject.gameObject.SetActive(!_isAlive);
+        _aliveObject.gameObject.SetActive(_isAlive);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GGJ2020/Assets/prefabs/DeadAliveCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadAliveCollider : MonoBehaviour
+{
+    [SerializeField]
+    private SphereCollider _collider;
+    
+    void Start()
+    {
+            
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.LogWarning("Colliding from Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
